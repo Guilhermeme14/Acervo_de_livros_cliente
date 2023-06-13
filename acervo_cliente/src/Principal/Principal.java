480515c20606e1c5a4b65e5cdca0767be3541ebf
Guilherme Fernandes <guifernandes111@gmail.com>
2023-06-12 22:40:14 -0300

    Add files via upload

---
+++ acervo_cliente/src/Principal/Principal.java
@@ -0,0 +1,15 @@
+package Principal;
+
+import javax.swing.*;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ClienteGUI();
+            }
+        });
+    }
+}
